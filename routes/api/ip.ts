6d3d694c72d3a93e7bcaf88d72297525fe0d7fd2
Oskar Okuno <2677573+MrOggy85@users.noreply.github.com>
2022-09-20 15:25:39 +0900

    add whois

--- routes/api/ip.ts
+++ routes/api/ip.ts
@@ -6,11 +6,24 @@ function assertIsNetAddr(addr: Deno.Addr): asserts addr is Deno.NetAddr {
   }
 }
 
-export const handler = (_req: Request, _ctx: HandlerContext): Response => {
+async function getCountry(hostname: string) {
+  const p = Deno.run({
+    cmd: ["whois", hostname],
+    stdout: "piped",
+   });
+
+   const output = await p.output() // "piped" must be set
+   const outStr = new TextDecoder().decode(output);
+   return outStr;
+}
+
+export const handler = async (_req: Request, _ctx: HandlerContext): Response => {
   _ctx.remoteAddr;
   assertIsNetAddr(_ctx.remoteAddr);
   const { hostname, port } = _ctx.remoteAddr;
+  const whois = await getCountry(hostname)
   const message =
-    `You connected from the following address: ${hostname} port: ${port}`;
+    `You connected from the following address: ${hostname} port: ${port}\n\n`
+    + `whois: ${whois}`
   return new Response(message);
 };

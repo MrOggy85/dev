b6f038862d78d1e9585c1f4f52b9a7a9d88bd10c
Oskar Okuno <2677573+MrOggy85@users.noreply.github.com>
2022-09-20 16:27:36 +0900

    switch to ipstack

--- routes/api/ip.ts
+++ routes/api/ip.ts
@@ -6,24 +6,134 @@ function assertIsNetAddr(addr: Deno.Addr): asserts addr is Deno.NetAddr {
   }
 }
 
-async function getCountry(hostname: string) {
-  const p = Deno.run({
-    cmd: ["whois", hostname],
-    stdout: "piped",
-   });
+type IpStackResult = {
+  /**
+   * e.g. "110.4.192.213"
+   */
+  ip: string;
+  /**
+   * e.g. "ipv4";
+   */
+  type: string;
+  /**
+   * e.g. "AS",
+   */
+  continent_code: string;
+  /**
+   * e.g. "Asia",
+   */
+  continent_name: string;
+  /**
+   * e.g. "JP",
+   */
+  country_code: string;
+  /**
+   *  "e.g. Japan",
+   */
+  country_name: string;
+  /**
+   * e.g. "14",
+   */
+  region_code: string;
+  /**
+   * e.g. "Kanagawa",
+   */
+  region_name: string;
+  /**
+   * e.g. "Yokohama"
+   */
+  city: string;
+  /**
+   * e.g. "231-0004"
+   */
+  zip: string;
+  /**
+   * e.g. 35.44960021972656,
+   */
+  "latitude": number;
+  /**
+   * e.g. 139.63919067382812,
+   */
+  "longitude": number;
+  "location": {
+    /**
+     * e.g. 1848354
+     */
+    "geoname_id": number;
+    /**
+     * e.g. "Tokyo",
+     */
+    "capital": string;
+    "languages": {
+      /**
+       * e.g. "ja",
+       */
+      "code": string;
+      /**
+       * e.g. "Japanese",
+       */
+      "name": string;
+      /**
+       * e.g. "日本語"
+       */
+      "native": string;
+    }[];
 
-   const output = await p.output() // "piped" must be set
-   const outStr = new TextDecoder().decode(output);
-   return outStr;
+    /**
+     * e.g.  "https://assets.ipstack.com/flags/jp.svg",
+     */
+    "country_flag": string;
+    /**
+     * e.g.  "🇯🇵",
+     */
+    "country_flag_emoji": string;
+    /**
+     * e.g. "U+1F1EF U+1F1F5",
+     */
+    "country_flag_emoji_unicode": string;
+    /**
+     * e.g. "81",
+     */
+    "calling_code": string;
+
+    "is_eu": boolean;
+  };
+};
+
+function getKeys<T>(obj: Record<string, any>): (keyof T)[] {
+  return Object.keys(obj) as (keyof T)[];
 }
 
-export const handler = async (_req: Request, _ctx: HandlerContext): Response => {
+const ipStackAccessKey = Deno.env.get('IPSTACK_ACCESS_KEY')
+
+async function getCountry(hostname: string) {
+  const resp = await fetch(`http://api.ipstack.com/${hostname}?access_key=${ipStackAccessKey}`);
+  const json = await resp.json() as IpStackResult;
+  let txt = ''
+  getKeys<IpStackResult>(json).forEach(x => {
+    if (x === 'location') {
+      return;
+    }
+    let value = json[x];
+    if (!value) {
+      value = '-'
+    }
+
+    txt += `${x}: ${value}\n`
+  })
+  return txt
+}
+
+export const handler = async (
+  _req: Request,
+  _ctx: HandlerContext,
+): Promise<Response> => {
   _ctx.remoteAddr;
   assertIsNetAddr(_ctx.remoteAddr);
   const { hostname, port } = _ctx.remoteAddr;
-  const whois = await getCountry(hostname)
+  const whois = await getCountry(hostname);
   const message =
-    `You connected from the following address: ${hostname} port: ${port}\n\n`
-    + `whois: ${whois}`
+    `You connected from the following address: ${hostname} port: ${port}\n\n` +
+    `whois: ${whois}`;
   return new Response(message);
 };
